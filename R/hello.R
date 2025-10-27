9ca072ae69a74d2251ac1989d707048f8fa80997
Raja Rathour <imraja729@gmail.com>
2025-10-27 10:46:43 +0530

    feat: Add R directory with README.md and hello.R files (#573)
    Signed-off-by: Raja Rathour <imraja729@gmail.com>

---
+++ R/hello.R
@@ -0,0 +1,10 @@
+# Simple R script
+
+# Print a greeting to the console
+print("Hello, R language world!")
+
+# Example calculation
+x <- 5
+y <- 10
+result <- x + y
+print(paste("The sum of x and y is:", result))
