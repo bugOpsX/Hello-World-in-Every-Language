ede1251f996ec19b94c88f1a5c5407237694a036
bugOpsX <sumantkumar3172@gmail.com>
2025-11-11 14:43:27 +0530

    Managed all folders

--- find here!!/R/hello.R
+++ find here!!/R/hello.R
@@ -1,1 +1,10 @@
-print("Hello, World!")
+# Simple R script
+
+# Print a greeting to the console
+print("Hello, R language world!")
+
+# Example calculation
+x <- 5
+y <- 10
+result <- x + y
+print(paste("The sum of x and y is:", result))
